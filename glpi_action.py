4c90ecbaeb09f2e714171a372ed41f9741bcc21a
BorisovPV <89080897+BorisovPV@users.noreply.github.com>
2022-09-25 20:57:04 +0300

    Update glpi_action.py

--- glpi_action.py
+++ glpi_action.py
@@ -14,12 +14,3 @@ def add_comment_to_ticket(ticket, comment):
     glpi.createFollowup('ticket',
                         {"itemtype": "Ticket", "items_id": int(ticket), "tickets_id": int(ticket),
                          "content": str(comment)})
-
-
-def hidden_comment(ticket, comment):
-    glpi.createFollowup('ticket',
-                        {
-                            "itemtype": "Ticket", "items_id": int(ticket), "tickets_id": int(ticket), "is_private": "1",
-                            "content": str(comment)
-                        }
-                        )
